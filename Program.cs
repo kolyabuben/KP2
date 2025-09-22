f5a0993182a3d6a01b294d330e1cb478b7178617
kolyabuben <124598217+kolyabuben@users.noreply.github.com>
2025-09-23 01:38:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KP_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Decimal pi = 3.141592653m;
+            Decimal e = 2.7182818284590452m;
+            Console.WriteLine("П=" + pi);
+            Console.WriteLine("е=" + e);
+            Console.ReadKey();
+
+        }
+
+    }
+}
